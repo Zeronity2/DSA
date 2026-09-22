f24332b694786583b2efeadf9a0be117c755aadb
Khushi Patel <khushiwonder@gmail.com>
2026-09-22 17:59:41 +0530

    second largest element in an array

---
+++ Check2Largest.java
@@ -0,0 +1,19 @@
+public class Check2Largest {
+    public static void main(String[] args){
+        int[] arr={10,10,20,15,8};
+
+        int largest= Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int num:arr){
+            if(num>largest){
+                secondLargest = largest;
+                largest=num;
+            }
+            else if(num>secondLargest && num<largest){
+                secondLargest=num;
+            }
+        }
+        System.out.println("Second Largest Element: "+secondLargest);
+    }
+}
