90cbfaf278d877ae6eff7a0b2883f1683ec63cf3
Khushi Patel <khushiwonder@gmail.com>
2026-09-23 18:12:08 +0530

    frequency of an array

--- ArrayFrequency.java
+++ ArrayFrequency.java
@@ -1,5 +1,18 @@
 public class ArrayFrequency {
     public static void main(String[] args){
-        
+        int[] arr = {1, 2, 2, 3, 1, 1, 4};
+
+for (int i = 0; i < arr.length; i++) {
+
+    int count = 0;
+
+    for (int j = 0; j < arr.length; j++) {
+        if (arr[i] == arr[j]) {
+            count++;
+        }
+    }
+
+    System.out.println(arr[i] + " → " + count);
+}
     }
 }
